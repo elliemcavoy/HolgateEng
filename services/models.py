163b39c57ddab324222fde5ec95c0f6aea7eaa1c
elliemcavoy <ellie.mcavoy@outlook.com>
2022-06-05 20:35:14 +0000

    Added services model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.name
