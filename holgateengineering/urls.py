3748fee3aa91c6ce7c25115c778f51eb8a032457
elliemcavoy <ellie.mcavoy@outlook.com>
2022-06-06 14:08:33 +0000

    Added services html and views to render

--- holgateengineering/urls.py
+++ holgateengineering/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('contact/', include('contact.urls')),
+    path('services/', include('services.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- services/views.py
+++ services/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Service
 # Create your views here.
+
+def all_services(request):
+    """ View to render the menu page """
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services,
+    }
+    return render(request, 'services/services.html', context)
