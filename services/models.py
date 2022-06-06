aacc7983703636323bb786288f94682801b4a6c1
elliemcavoy <ellie.mcavoy@outlook.com>
2022-06-06 15:46:35 +0000

    Rendered services onto html page

--- services/models.py
+++ services/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Service(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='media')
 
     def __str__(self):
         return self.name
